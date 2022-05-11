6a236fa07ac9f1ece18209fe7ae770763b27cd6c
Ramond Falcão <ramondfalcao@gmail.com>
2022-05-11 20:11:47 -0300

    Updates

---
+++ precache-manifest.86ddd8062fd11d9f77dd5e0a07630a60.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee0a05ea7ee9221690203a5bd333b404",
+    "url": "/trivia-game/index.html"
+  },
+  {
+    "revision": "7f32a20191158e3bb2dd",
+    "url": "/trivia-game/static/css/main.453f2884.chunk.css"
+  },
+  {
+    "revision": "dd5fb242b0e6703815a7",
+    "url": "/trivia-game/static/js/2.6be9b1d5.chunk.js"
+  },
+  {
+    "revision": "ca6b5a6a129747daa0202cfb1dcfa4c4",
+    "url": "/trivia-game/static/js/2.6be9b1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f32a20191158e3bb2dd",
+    "url": "/trivia-game/static/js/main.ade99aaf.chunk.js"
+  },
+  {
+    "revision": "b0c6c90c513f51408a18",
+    "url": "/trivia-game/static/js/runtime-main.4bc9de50.js"
+  },
+  {
+    "revision": "bd98d43a21eb0816eee4d89d33a7d87c",
+    "url": "/trivia-game/static/media/happy.bd98d43a.gif"
+  },
+  {
+    "revision": "29b728778f1ca88a47874c73495681c1",
+    "url": "/trivia-game/static/media/logoBlue.29b72877.svg"
+  },
+  {
+    "revision": "001dc1c0917347abce1132cea81575f9",
+    "url": "/trivia-game/static/media/timerImg.001dc1c0.svg"
+  },
+  {
+    "revision": "719377f2cf33a49fe031e9f9535a9675",
+    "url": "/trivia-game/static/media/tristeza.719377f2.gif"
+  }
+]);
